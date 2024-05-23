fd930e6dd5901fd0aab33fe8767515b908b39546
KANHABOSSKING <168426938+KANHABOSSKING@users.noreply.github.com>
2024-05-23 06:58:43 +0530

    Update start.py.

--- STORM/modules/start.py
+++ STORM/modules/start.py
@@ -25,17 +25,17 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_OP = [
     [
-        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/Kexx_XD"),
-        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/STORM_CHATZ"),
+        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/DEADLY_KANHA"),
+        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/KANHA_FEELINGS"),
     ],
     [
         Button.url(
-            "ɢʀᴏᴜᴘ 🧸", "https://t.me/FriendCastel"
+            "ɢʀᴏᴜᴘ 🧸", "https://t.me/KANHA_FEELINGS"
         ),
     ],
     [
-        Button.url("ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ❄️", "https://github.com/VARC9210/STORM"),
-        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/STORM_TECHH"),
+        Button.url("ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ❄️", "https://t.me/KANHA_FEELINGS"),
+        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/KANHA_FEELINGS"),
     ],
 ]
 
@@ -55,7 +55,7 @@ async def start(event):
         bot_name = KEX.first_name
         bot_id = KEX.id
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}]\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n➖➖➖➖➖➖➖➖➖➖➖\n"
-        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [⏤͟͞〲ᴋᴇx](https://t.me/kexx_xd)**\n"
+        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [⏤͟͞〲ᴋᴇx](https://t.me/deadly_kanha)**\n"
         TEXT += f"» **ꜱᴛᴏʀᴍ :** `M3.0` \n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ :** `3.11` \n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ :** `{__version__}`\n➖➖➖➖➖➖➖➖➖➖➖"        
